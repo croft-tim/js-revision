5ac4dfe3d8b921b039c0a33394c6051e2e6cbba4
Tim Croft <timdcroft@gmail.com>
2026-02-11 11:19:46 +1300

    completed isValidlog part

--- src/helpers.js
+++ src/helpers.js
@@ -3,7 +3,22 @@
 
 export function isValidLog(log) {
   //if (typeof log.user === 'string') return true
+  //we really only want the false statement because all the others are true
   if (typeof log.user !== 'string') return false
+  //here we want to check that the string is greater than 0
+  if (log.user.trim() === 0) return false
+  // here we want to see if time isn't a number return false
+  if (typeof log.time !== 'number') return false
+  // here we want to see if time is less than 0 then return false
+  if (log.time < 0) return false
+  // here we want to see if the log action does not equal these things then it is false
+  if (
+    log.action !== 'login' &&
+    log.action !== 'click' &&
+    log.action !== 'logout'
+  )
+    return false
+  return true
 }
 
 export function normaliseLog(log) {
