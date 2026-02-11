a977f4fccde8f31c5bc8ab8c65033361a371baa5
Tim Croft <timdcroft@gmail.com>
2026-02-11 14:14:32 +1300

    first processActivityLog

--- src/processActivityLogs.js
+++ src/processActivityLogs.js
@@ -1,11 +1,27 @@
 import { isValidLog, normaliseLog, groupByUser } from './helpers.js'
 
 export function processActivityLogs(logs) {
+  // logs === logs array - expects a singular
   // Step 1: validate logs
-  // Step 2: normalise data
-  // Step 3: group by user
-  // Step 4: calculate per-user stats
-  // Step 5: calculate global stats
-  // Step 6: generate insights
-  // Step 7: return final result object
+
+  const validLogs = []
+  const inValidLogs = []
+
+  // loop
+
+  for (let i = 0; i < logs.length; i++)
+    if (isValidLog(logs[i])) {
+      // if true do this
+
+      validlogs.push(logs[i]) // add to the end of the array
+    } else {
+      inValidLogs.push(logs[i]) // we have run a valid logs so since we have this information, use DRY and export invalid too (we may want this at some point)
+    }
 }
+
+// Step 2: normalise data
+// Step 3: group by user
+// Step 4: calculate per-user stats
+// Step 5: calculate global stats
+// Step 6: generate insights
+// Step 7: return final result object
